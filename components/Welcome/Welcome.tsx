a01bd727a2d683eba2791644d7f10a588cc3b5cf
ennea8 <ennea8@gmail.com>
2025-04-24 17:19:43 +0800

    update homepage

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -5,19 +5,19 @@ export function Welcome() {
   return (
     <>
       <Title className={classes.title} ta="center" mt={100}>
-        Welcome to{' '}
+        {/* Welcome to{' '} */}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
+        Welcome
         </Text>
       </Title>
-      <Text color="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
+      {/* <Text color="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
         This starter Next.js project includes a minimal setup for server side rendering, if you want
         to learn more on Mantine + Next.js integration follow{' '}
         <Anchor href="https://mantine.dev/guides/next/" size="lg">
           this guide
         </Anchor>
         . To get started edit index.tsx file.
-      </Text>
+      </Text> */}
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function HomePage () {
         <ConnectButton />
       </div>
       <Welcome />
-      <ColorSchemeToggle />
+      {/* <ColorSchemeToggle /> */}
     </>
   )
 }
