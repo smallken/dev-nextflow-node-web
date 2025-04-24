9af1834c90d2fb2df12b0e0b5a65c31859307d7e
ennea8 <ennea8@gmail.com>
2025-04-24 17:09:44 +0800

    add wallet button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { Welcome } from '../components/Welcome/Welcome';
+import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle'
+import { Welcome } from '../components/Welcome/Welcome'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
 
-export default function HomePage() {
+export default function HomePage () {
   return (
     <>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          padding: 12
+        }}
+      >
+        <ConnectButton />
+      </div>
       <Welcome />
       <ColorSchemeToggle />
     </>
-  );
+  )
 }
