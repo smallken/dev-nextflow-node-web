e938aaecbcd1b49dd671d132ece679dd27813655
ennea8 <ennea8@gmail.com>
2025-04-25 12:15:29 +0800

    update theme

--- theme.ts
+++ theme.ts
@@ -1,5 +1,22 @@
-import { createTheme } from '@mantine/core';
+import { createTheme, MantineColorsTuple } from '@mantine/core';
+
+const myColor: MantineColorsTuple = [
+  '#e5fff1',
+  '#d2fae5',
+  '#a8f2cb',
+  '#7aeaaf',
+  '#54e397',
+  '#3bdf88',
+  '#2bdd80',
+  '#1ac46d',
+  '#08ae5f',
+  '#00974f'
+];
 
 export const theme = createTheme({
   /* Put your mantine theme override here */
+  colors: {
+    myColor,
+  },
+  primaryColor: 'myColor'
 });
