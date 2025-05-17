9c4ea54948e1c0ea4c170cbf68338502ac4abddb
ennea8 <ennea8@gmail.com>
2025-05-17 15:07:24 +0800

    update title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -40,7 +40,7 @@ export default function App ({ Component, pageProps }: AppProps) {
           >
             <UserProvider>
               <Head>
-                <title>FF Node</title>
+                <title>FlipFlop Never Node</title>
                 <meta
                   name='viewport'
                   content='minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no'
